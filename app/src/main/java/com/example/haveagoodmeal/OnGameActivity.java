61029e850262d9fd479366761880db207856c426
imseeun0626 <imseeun0626@gmail.com>
2023-06-10 00:13:03 +0900

    배경화면 추가

---
+++ app/src/main/java/com/example/haveagoodmeal/OnGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.haveagoodmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_game);
+    }
+}
