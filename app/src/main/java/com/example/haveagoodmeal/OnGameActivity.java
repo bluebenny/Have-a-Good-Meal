d8a7cb03ada551a826b2ce58fe4edaf52cf7ee85
HyeonBin Park <redxg1229@gmail.com>
2023-06-20 21:54:17 +0900

    모든 재료 버튼 코드 생성

--- app/src/main/java/com/example/haveagoodmeal/OnGameActivity.java
+++ app/src/main/java/com/example/haveagoodmeal/OnGameActivity.java
@@ -294,42 +294,260 @@ public class OnGameActivity extends AppCompatActivity {
         for (String str : ingredientNames)
             ingredientValues.put(str, 0);
 
-        TextView text = findViewById(R.id.text);
+        /* ingredient buttons (28 things) */
+        {
+            Button buttonWater = findViewById(R.id.button_water);
+            buttonWater.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("water", ingredientValues.get("water") + 1);
+                }
+            });
 
-        Button buttonWater = findViewById(R.id.button_water);
-        buttonWater.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ingredientValues.put("water", ingredientValues.get("water") + 1);
-            }
-        });
+            Button buttonSalt = findViewById(R.id.button_salt);
+            buttonSalt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("salt", ingredientValues.get("salt") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_salt);
+                }
+            });
 
-        Button buttonSalt = findViewById(R.id.button_salt);
-        buttonSalt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ingredientValues.put("salt", ingredientValues.get("salt") + 1);
-                rotateImageViewer.setImage(R.drawable.image_salt);
-            }
-        });
+            Button buttonSugar = findViewById(R.id.button_sugar);
+            buttonSugar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("sugar", ingredientValues.get("sugar") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_sugar);
+                }
+            });
 
-        Button buttonCarrot = findViewById(R.id.button_carrot);
-        buttonCarrot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ingredientValues.put("carrot", ingredientValues.get("carrot") + 1);
-                rotateImageViewer.setImage(R.drawable.image_carrot);
-            }
-        });
+            Button buttonSoySauce = findViewById(R.id.button_soysauce);
+            buttonSoySauce.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("soysauce", ingredientValues.get("soysauce") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_soysauce);
+                }
+            });
 
-        Button buttonOnion = findViewById(R.id.button_onion);
-        buttonOnion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ingredientValues.put("onion", ingredientValues.get("onion") + 1);
-                rotateImageViewer.setImage(R.drawable.image_onion);
-            }
-        });
+            Button buttonPepperPaste = findViewById(R.id.button_pepperpaste);
+            buttonPepperPaste.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("pepperpaste", ingredientValues.get("pepperpaste") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_pepperpaste);
+                }
+            });
+
+            Button buttonPepperPowder = findViewById(R.id.button_pepperpowder);
+            buttonPepperPowder.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("pepperpowder", ingredientValues.get("pepperpowder") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_pepperpowder);
+                }
+            });
+
+            Button buttonSyrup = findViewById(R.id.button_syrup);
+            buttonSyrup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("syrup", ingredientValues.get("syrup") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_syrup);
+                }
+            });
+
+            Button buttonOligo = findViewById(R.id.button_oligo);
+            buttonOligo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("oligo", ingredientValues.get("oligo") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_oligo);
+                }
+            });
+
+            Button buttonSesameOil = findViewById(R.id.button_sesameoil);
+            buttonSesameOil.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("sesameoil", ingredientValues.get("sesameoil") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_sesameoil);
+                }
+            });
+
+            Button buttonPepper = findViewById(R.id.button_pepper);
+            buttonPepper.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("pepper", ingredientValues.get("pepper") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_pepper);
+                }
+            });
+
+            Button buttonMayo = findViewById(R.id.button_mayo);
+            buttonMayo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("mayo", ingredientValues.get("mayo") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_mayo);
+                }
+            });
+
+            Button buttonJam = findViewById(R.id.button_jam);
+            buttonJam.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("jam", ingredientValues.get("jam") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_jam);
+                }
+            });
+
+            Button buttonCarrot = findViewById(R.id.button_carrot);
+            buttonCarrot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("carrot", ingredientValues.get("carrot") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_carrot);
+                }
+            });
+
+            Button buttonOnion = findViewById(R.id.button_onion);
+            buttonOnion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("onion", ingredientValues.get("onion") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_onion);
+                }
+            });
+
+            Button buttonGreenOnion = findViewById(R.id.button_greenonion);
+            buttonGreenOnion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("greenonion", ingredientValues.get("greenonion") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_greenonion);
+                }
+            });
+
+            Button buttonLeek = findViewById(R.id.button_leek);
+            buttonLeek.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("leek", ingredientValues.get("leek") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_leek);
+                }
+            });
+
+            Button buttonTofu = findViewById(R.id.button_tofu);
+            buttonTofu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("tofu", ingredientValues.get("tofu") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_tofu);
+                }
+            });
+
+            Button buttonCucumber = findViewById(R.id.button_cucumber);
+            buttonCucumber.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("cucumber", ingredientValues.get("cucumber") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_cucumber);
+                }
+            });
+
+            Button buttonTomato = findViewById(R.id.button_tomato);
+            buttonTomato.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("tomato", ingredientValues.get("tomato") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_tomato);
+                }
+            });
+
+            Button buttonEgg = findViewById(R.id.button_egg);
+            buttonEgg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("egg", ingredientValues.get("egg") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_egg);
+                }
+            });
+
+            Button buttonSeaweed = findViewById(R.id.button_seaweed);
+            buttonSeaweed.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("seaweed", ingredientValues.get("seaweed") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_seaweed);
+                }
+            });
+
+            Button buttonPotato = findViewById(R.id.button_potato);
+            buttonPotato.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("potato", ingredientValues.get("potato") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_potato);
+                }
+            });
+
+            Button buttonSweetPotato = findViewById(R.id.button_sweetpotato);
+            buttonSweetPotato.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("sweetpotato", ingredientValues.get("sweetpotato") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_sweetpotato);
+                }
+            });
+
+            Button buttonDoubanjiang = findViewById(R.id.button_doubanjiang);
+            buttonDoubanjiang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("doubanjiang", ingredientValues.get("doubanjiang") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_doubanjiang);
+                }
+            });
+
+            Button buttonMeat = findViewById(R.id.button_meat);
+            buttonMeat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("meat", ingredientValues.get("meat") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_meat);
+                }
+            });
+
+            Button buttonTuna = findViewById(R.id.button_tuna);
+            buttonTuna.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("tuna", ingredientValues.get("tuna") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_tuna);
+                }
+            });
+
+            Button buttonVegetable1 = findViewById(R.id.button_vegetable1);
+            buttonVegetable1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("vegetable1", ingredientValues.get("vegetable1") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_vegetable1);
+                }
+            });
+
+            Button buttonVegetable2 = findViewById(R.id.button_vegetable2);
+            buttonVegetable2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ingredientValues.put("vegetable2", ingredientValues.get("vegetable2") + 1);
+                    rotateImageViewer.setImage(R.drawable.image_vegetable2);
+                }
+            });
+
+        }
 
         Button next = findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
