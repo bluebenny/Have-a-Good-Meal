b3ee4ac552f690ce223cf6e1ced5ab368c3151bf
yuly0129 <yulsheep2452+1@gmail.com>
2023-06-20 22:49:14 +0900

    시작 화면 생성 및 디자인

---
+++ app/src/main/java/com/example/haveagoodmeal/StartActivity.java
@@ -0,0 +1,51 @@
+package com.example.haveagoodmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+
+public class StartActivity extends AppCompatActivity {
+    private ImageView startImageView;
+    private ImageView titleImageView;
+
+    private  Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitynext();
+            }
+        });
+
+
+    }
+
+    public void openActivitynext() {
+        Intent intent = new Intent(this, OnGameActivity.class);
+        startActivity(intent);
+
+    }
+
+}
