810581938d4eb94ca671c5c91e5a46c4df3e4af4
연우 <wjddusdn011218@gmail.com>
2023-06-20 21:17:54 +0900

    순환 이미지뷰 클래스 생성 및 메소드 적용

--- app/src/main/java/com/example/haveagoodmeal/OnGameActivity.java
+++ app/src/main/java/com/example/haveagoodmeal/OnGameActivity.java
@@ -7,9 +7,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OnGameActivity extends AppCompatActivity {
+
+    class RotateImageViewer {
+
+        int[] ids = new int[8];
+
+        ImageView[] images = new ImageView[] {
+                (ImageView) findViewById(R.id.ingredient8),
+                (ImageView) findViewById(R.id.ingredient7),
+                (ImageView) findViewById(R.id.ingredient6),
+                (ImageView) findViewById(R.id.ingredient5),
+                (ImageView) findViewById(R.id.ingredient4),
+                (ImageView) findViewById(R.id.ingredient3),
+                (ImageView) findViewById(R.id.ingredient2),
+                (ImageView) findViewById(R.id.ingredient1)
+        };
+
+        RotateImageViewer() {}
+
+        public void setImage(int id) {
+            for (int i = 7; i > 0; i--)
+                ids[i] = ids[i - 1];
+            ids[0] = id;
+
+            for (int i = 0; i < 8; i++)
+                images[i].setImageResource(ids[i]);
+        }
+
+    }
 
     Integer score = 0;
 
@@ -18,13 +47,14 @@ public class OnGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_game);
 
+        RotateImageViewer rotateImageViewer = new RotateImageViewer();
+
         TextView text = findViewById(R.id.text);
 
         Button buttonWater = findViewById(R.id.button_water);
         buttonWater.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text.setText("Water");
                 score += 5;
             }
         });
@@ -34,6 +64,7 @@ public class OnGameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 text.setText("Salt");
+                rotateImageViewer.setImage(R.drawable.image_salt);
                 score += 10;
             }
         });
@@ -43,6 +74,7 @@ public class OnGameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 text.setText("Carrot");
+                rotateImageViewer.setImage(R.drawable.image_carrot);
                 score += 10;
             }
         });
@@ -52,6 +84,7 @@ public class OnGameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 text.setText("Onion");
+                rotateImageViewer.setImage(R.drawable.image_onion);
                 score += 10;
             }
         });
